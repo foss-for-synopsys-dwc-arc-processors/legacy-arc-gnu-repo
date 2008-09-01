bc711f048e55dabe6a202fca043bd54a5bb84d02
irfanr <irfanr@cc1849f3-3b6f-4791-a2b5-460ca66dfaf0>
2008-09-01 16:26:16 +0000

    statfs fix
    git-svn-id: http://sjvm-subversion02/svn/OSS/releases/2.0/trunk@57092 cc1849f3-3b6f-4791-a2b5-460ca66dfaf0

--- bootstrap/bits/statfs.h
+++ bootstrap/bits/statfs.h
@@ -26,42 +26,81 @@ struct statfs
   {
     __SWORD_TYPE f_type;
     __SWORD_TYPE f_bsize;
+
 #ifndef __USE_FILE_OFFSET64
     __fsblkcnt_t f_blocks;
     __fsblkcnt_t f_bfree;
     __fsblkcnt_t f_bavail;
     __fsfilcnt_t f_files;
     __fsfilcnt_t f_ffree;
+
 #else
+    __SWORD_TYPE f_frsize;
+    __SWORD_TYPE __pad;
     __fsblkcnt64_t f_blocks;
     __fsblkcnt64_t f_bfree;
-    __fsblkcnt64_t f_bavail;
     __fsfilcnt64_t f_files;
     __fsfilcnt64_t f_ffree;
+    __fsblkcnt64_t f_bavail;
 #endif
     __fsid_t f_fsid;
     __SWORD_TYPE f_namelen;
-    __SWORD_TYPE f_frsize;
-    __SWORD_TYPE f_spare[5];
+    __SWORD_TYPE f_spare[6];
   };
 
+/* linux26 STRUCTURE (as in svn trunk as on 27 Aug 2008)
+struct statfs {
+	long		f_type;
+	long		f_bsize;
+	long		f_blocks;
+	long		f_bfree;
+	long		f_bavail;
+	long		f_files;
+	long		f_ffree;
+
+	/* Linux specials 
+	__kernel_fsid_t	f_fsid;
+	long		f_namelen;
+	long		f_spare[6];
+
+	long		f_frsize;	/* Fragment size - unsupported 
+
+};
+*/
 #ifdef __USE_LARGEFILE64
 struct statfs64
   {
     __SWORD_TYPE f_type;
     __SWORD_TYPE f_bsize;
+    __SWORD_TYPE f_frsize;
+    __SWORD_TYPE __pad;
     __fsblkcnt64_t f_blocks;
     __fsblkcnt64_t f_bfree;
-    __fsblkcnt64_t f_bavail;
     __fsfilcnt64_t f_files;
     __fsfilcnt64_t f_ffree;
+    __fsblkcnt64_t f_bavail;
     __fsid_t f_fsid;
     __SWORD_TYPE f_namelen;
-    __SWORD_TYPE f_frsize;
-    __SWORD_TYPE f_spare[5];
+    __SWORD_TYPE f_spare[6];
   };
 #endif
 
+/* linux26 STRUCTURE (as in svn trunk as on 27 Aug 2008)
+struct statfs64 {
+	__u32	f_type;
+	__u32	f_bsize;
+	__u32	f_frsize;	/* Fragment size - unsupported 
+	__u32	__pad;
+	__u64	f_blocks;
+	__u64	f_bfree;
+	__u64	f_files;
+	__u64	f_ffree;
+	__u64	f_bavail;
+	__kernel_fsid_t f_fsid;
+	__u32	f_namelen;
+	__u32	f_spare[6];
+};
+*/
 /* Tell code we have these members.  */
 #define _STATFS_F_NAMELEN
 #define _STATFS_F_FRSIZE
