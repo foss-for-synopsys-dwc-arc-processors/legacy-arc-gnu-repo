92320132afb96df953496b0d77d9a9d94c3fdba2
irfanr <irfanr@cc1849f3-3b6f-4791-a2b5-460ca66dfaf0>
2008-10-09 16:04:23 +0000

    clean elf32/uclibc toolchain
    git-svn-id: http://sjvm-subversion02/svn/OSS/releases/2.0/trunk@59900 cc1849f3-3b6f-4791-a2b5-460ca66dfaf0

---
+++ clean.sh
@@ -0,0 +1,19 @@
+# This script cleans up ELF32/UCLIBC toolchain builds
+
+
+rm -rf ./binutils/build
+rm -rf ./gcc/build
+rm -rf ./insight/build
+
+mkdir binutils/build
+mkdir gcc/build
+mkdir insight/build
+
+cd uClibc-0.9.29
+make clean > /dev/zero 2> /dev/zero
+cd ..
+
+
+
+
+ 
