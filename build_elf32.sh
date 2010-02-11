9e80711099ccf84d9966c3e14e44b227ed0acf54
vineetg <vineetg@cc1849f3-3b6f-4791-a2b5-460ca66dfaf0>
2010-02-11 01:54:45 +0000

    Minor cleanup of build script to delete old objs
    git-svn-id: http://sjvm-subversion02/svn/OSS/releases/2.0/trunk@92983 cc1849f3-3b6f-4791-a2b5-460ca66dfaf0

--- build_elf32.sh
+++ build_elf32.sh
@@ -12,7 +12,7 @@
 INSTALLDIR=$1
 
 #build binutils
-mkdir -p binutils/build gcc/build insight/build
+rm -rf binutils/build && mkdir binutils/build
 cd binutils/build
 ../src/configure --prefix=$INSTALLDIR --target=arc-elf32 --disable-werror  > ../../log.txt 2>> ../../log.txt
 make  >> ../../log.txt 2>> ../../log.txt
@@ -28,6 +28,7 @@ export NM_FOR_TARGET=arc-elf32-nm
 export RANLIB_FOR_TARGET=arc-elf32-ranlib
 
 #build gcc
+rm -rf gcc/build && mkdir gcc/build
 cd gcc/build
 ../src/configure --target=arc-elf32 --prefix=$INSTALLDIR --with-headers --enable-multilib --with-newlib --enable-languages=c,c++ --disable-shared  >> ../../log.txt 2>> ../../log.txt
 make  >> ../../log.txt 2>> ../../log.txt
@@ -40,6 +41,7 @@ cp $INSTALLDIR/lib/libs*.a $INSTALLDIR/arc-elf32/lib/
 cp $INSTALLDIR/lib/arc700/libs*.a $INSTALLDIR/arc-elf32/lib/arc700/
 
 #build insight
+rm -rf insight/build && mkdir insight/build
 cd insight/build
 ../src/configure --target=arc-elf32 --prefix=$INSTALLDIR --disable-werror  >> ../../log.txt 2>> ../../log.txt
 make  >> ../../log.txt 2>> ../../log.txt
