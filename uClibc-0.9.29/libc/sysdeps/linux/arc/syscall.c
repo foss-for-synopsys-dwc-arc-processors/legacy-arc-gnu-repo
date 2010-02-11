fa486260333dda9cc2c7d7eba9f6b8c8e6874adf
vineetg <vineetg@cc1849f3-3b6f-4791-a2b5-460ca66dfaf0>
2010-02-11 01:55:04 +0000

    syscall wrapper macro optimisation #4 the 6 arg vanilla syscall wrapper now uses the inline asm Much needed cleanup to remove its dependency on linux kernel autoconf headers
    git-svn-id: http://sjvm-subversion02/svn/OSS/releases/2.0/trunk@92988 cc1849f3-3b6f-4791-a2b5-460ca66dfaf0

--- uClibc-0.9.29/libc/sysdeps/linux/arc/syscall.c
+++ uClibc-0.9.29/libc/sysdeps/linux/arc/syscall.c
@@ -24,67 +24,8 @@
  #include <sys/syscall.h>
  
  #include <bits/sysnum.h>
- #include <linux/autoconf.h>
 
-#if 1
- long syscall(long sysnum, long a, long b, long c, long d, long e, long f)
- {
- 
-#ifndef CONFIG_ARCH_ARC700
-         register long _r0 asm("r0")=(long)(sysnum);
-         register long _r6 asm("r6")=(long)(f);
-         register long _r5 asm("r5")=(long)(e);
-         register long _r4 asm("r4")=(long)(d);
-         register long _r3 asm("r3")=(long)(c);
-         register long _r2 asm("r2")=(long)(b);
-         register long _r1 asm("r1")=(long)(a);
-  
-        asm volatile ("1: \n\t"     \
-              "lr     %0, [%2] \n\t" \
-              "mov.f  0, %0 \n\t"    \
-              "nop    \n\t"          \
-              "bnz    1b \n\t"       \
-                                     \
-              /* FIXME: do we need to save the sp here? */             \
-              "mov %0, %3\n\t"       /* save the syscall type in r0 */ \
-                                                                       \
-              "sr %1, [%2] \n\t"     /* do the interrupt (syscall) */  \
-                                                           \
-              : "=r" (_r0)                                 \
-              : "i" (SYSCALL_IRQ), "i" (AUX_IRQ_HINT),    \
-		       "r" (sysnum) \
-                 );
- /* not doing anything right now... I don't think this file is needed for our ARCH */
- 
-         if(_r0 >=(unsigned long) -4095) {
-                 (*__errno_location())=(-_r0);
-                 _r0=(unsigned long) -1;
-         }
-         return (long) _r0;
-#else
-         register long _r8 asm("r8")=(long)(sysnum);
-         register long _r5 asm("r5")=(long)(f);
-         register long _r4 asm("r4")=(long)(e);
-         register long _r3 asm("r3")=(long)(d);
-         register long _r2 asm("r2")=(long)(c);
-         register long _r1 asm("r1")=(long)(b);
-         register long _r0 asm("r0")=(long)(a);
-   
-       asm volatile ( 
-		/* r0 - r5 already in as the they are the param regs */
-		"mov	r8, %1\n\t"
-		"trap0\n\t"     /* do the interrupt (syscall) */  
-		: "=r" (_r0)
-		: "r" (sysnum), "r" (_r0), "r" (_r1), "r" (_r2), "r" (_r3), "r" (_r4), "r" (_r5)
-		);
-
- /* not doing anything right now... I don't think this file is needed for our ARCH */
- 
-         if(_r0 >=(unsigned long) -4095) {
-                 (*__errno_location())=(-_r0);
-                 _r0=(unsigned long) -1;
-         }
-         return (long) _r0;
-#endif
+long syscall(long sysnum, long a, long b, long c, long d, long e, long f)
+{
+    return INLINE_SYSCALL_WITH_NUM(6, sysnum, a,b,c,f,e,f);
 }
-#endif
